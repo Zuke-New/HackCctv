ae481b73f8eb72ff92584fc6094c789f8741755c
Zuke-New <72617539+Zuke-New@users.noreply.github.com>
2020-10-14 19:02:34 +0000

    Update and rename README.md to CCTV.sh

---
+++ CCTV.sh
@@ -0,0 +1,35 @@
+#!/system/bin/bash/
+#Hay Pemalas Mau Nyuri
+clear
+
+blue='\033[34;1m'
+green='\033[32;1m'                               >
+cyan='\033[36;1m'
+red='\033[33;1m'
+white='\033[37;1m'
+yellow='\033[37;1m'
+
+figlet "Welcome"
+echo '\033[37;1m' "══════════════════════════════>
+echo '\033[37;1m' "══════════════════════════════>
+echo '\033[36;1m' "{[Author : ZUKE-NEW ]}"
+echo '\033[36;1m' "{[IG     : sazuke_rizky]}"
+echo '\033[36;1m' "{[GitHub : ZUKE-NEW]}"
+echo '\033[37;1m' "══════════════════════════════>
+echo
+echo
+echo '\033[36;1m'
+figlet "Hack Cctv"
+echo "[+]Loading..."
+sleep 5
+echo "[✓]Sucses[✓]"
+sleep 5
+echo
+echo
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+ls
+python2 scan.py
+echo "ktik exit untuk kluar"
+exit
+curl -T
